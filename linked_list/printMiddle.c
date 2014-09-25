6cf0302bd2dba47be53a959eb7becdc8f89a2b54
Ishan Sharma <ishansharma.in@gmail.com>
2014-09-25 15:34:40 +0400

    printMiddle.c finalized

--- linked_list/printMiddle.c
+++ linked_list/printMiddle.c
@@ -7,6 +7,23 @@ typedef struct listNode {
 	struct listNode *next;
 }listNode;
 
+/* Prints the middle element of the linked list */
+int printMiddle(listNode *head) {
+	listNode *slowP = head;
+	listNode *fastP = head;
+	
+	/* Check if the list is null */
+	if(head == NULL)
+		return -1;
+
+	while(fastP != NULL && fastP->next != NULL) {
+		slowP = slowP->next;
+		fastP = fastP->next->next;
+	}
+
+	return slowP->data;
+}
+
 listNode *getNode() {
 	listNode *newNode = (listNode *)malloc(sizeof(listNode));
 	newNode->data = -1;
@@ -22,6 +39,7 @@ void push(listNode **head, int data) {
 	*head = newNode;
 }
 
+/* Prints the linked list */
 int printList(listNode *head) {
 	if(head == NULL)
 		return -1;
@@ -33,20 +51,20 @@ int printList(listNode *head) {
 	return 0;
 }
 
-int printMiddle(listNode *head) {
-	if(head == NULL)
-		return -1;
-
-	
-}
 int main() {
 	listNode *head = NULL;
 	int i = 0;
+	int middle;
 
-	for(i=0; i<10;i++)
+	for(i=0; i<11;i++)
 		push(&head,i);
 
 	printList(head);
+
+	middle = printMiddle(head);
+	
+	if(middle >= 0)
+		printf("Middle element is %d\n",middle);
 
 	return 0;
 }
