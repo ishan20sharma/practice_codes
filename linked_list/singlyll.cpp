6d45dda9e608c588b79d5aac7c2e9165a1dd3be6
Ishan Sharma <ishansharma.in@gmail.com>
2014-10-02 09:56:21 +0400

    Added methods in singlyll.cpp

--- linked_list/singlyll.cpp
+++ linked_list/singlyll.cpp
@@ -18,7 +18,23 @@ class singlyll {
 	void insertHead(int data);
 	void insertEnd(int data);
 	int insertAt(int index, int data);
-
+	int insertSorted(); /* Insert element in a sorted fashion */
+	int deleteHead();
+	int deleteEnd();
+	int deleteAt(int index);
+	int deleteElement(int data);
+	int printMiddle();
+	int checkCycle();
+	int removeCycle(); /* Removes a cycle from the linked list */
+	int detectIntersection(); /* Get the intersection point of of two linked list */
+	int preintReverse(); /* Prints the element of linked list in reverse order */
+	int reverseUsingRecursion(); /* Reverse a linked list using recursion */
+	int removeDuplicates(); /* Removes duplicate elements */
+	int deleteAlternateNodes(); /* Delete alternate nodes of a linked list */
+	int reverseKNodes(); /* Reverse every k nodes of a linked list */
+	int rotateKNodes(); /* Rotate linked list by K nodes */
+	int mergeAtAlternate(); /* Merge a linked list into another list at alternate positions */
+	int printNFromEnd(); /* Print nth node from the end */
 };
 
 /* Default Constructor */
@@ -72,13 +88,32 @@ void singlyll::insertEnd(int data) {
 int singlyll::insertAt(int index, int data) {
 	ListNode *current = head;
 	ListNode *newNode = getNode(data);
+	int count = 1;
 
 	/* Check if the node is to be inserted at the first position */
-	if(head == NULL && index ==1) {
-
+	if(head == NULL && index != 1) {
+		return 0;
+	}
+	else if (index == 1) {
+		newNode->next = head;
+		head = newNode;
+		return 1;
 	}
 
-	return 1;
+	/* Traverse the linked list to locate the index position */
+	for(current; (current->next != NULL) && (count < index - 1); ++count, current = current->next) {
+		;
+	}
+
+	if(count == index -1) {
+		cout << "Inserting newnode at index-- " << count+1 << endl;
+		newNode->next = current->next;
+		current->next = newNode;
+		cout << "Node successfully inserted" << endl;
+		return 1;
+	}	
+	
+	return 0;
 }
 
 /* Prints the singly linked list */
@@ -105,6 +140,7 @@ int main() {
 		//list.insertHead(i);
 		list.insertEnd(i);
 	}
+	list.insertAt(56,16);
 
 	list.printList();
 
